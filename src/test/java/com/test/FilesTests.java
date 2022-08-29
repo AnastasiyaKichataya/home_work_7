7425fcee11f303479447954e8a3c857e2558764d
KichatayaAB <a.kichataya@w1.money>
2022-08-29 22:05:35 +0300

    new copy-test

--- src/test/java/com/test/FilesTests.java
+++ src/test/java/com/test/FilesTests.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +9,12 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilesTests {
@@ -20,26 +22,65 @@ public class FilesTests {
     ClassLoader classLoader = FilesTests.class.getClassLoader();
 
     @Test
-    void zipTest() throws Exception {
-        ZipFile zf = new ZipFile(new File("src/test/resources/files/Downloads.zip"));
-        ZipInputStream is = new ZipInputStream(classLoader.getResourceAsStream("files/Downloads.zip"));
+    void zipCsvReader() throws Exception {
+        InputStream is = classLoader.getResourceAsStream("Downloads.zip");
+        ZipInputStream zip = new ZipInputStream(is);
         ZipEntry entry;
-        while ((entry = is.getNextEntry()) != null) {
-            //org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo("example1.csv");
-            try (InputStream stream = zf.getInputStream(entry)){
-                CSVReader reader = new CSVReader(new InputStreamReader(stream, StandardCharsets.UTF_8));{
-                    List<String[]> csv = reader.readAll();
+        while ((entry = zip.getNextEntry()) != null) {
+            if (entry.getName().contains("example1.csv")) {
+                try {
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(zip, UTF_8));
+                    List<String[]> csv = csvReader.readAll();
                     assertThat(csv).contains(
+                            new String[]{"Date", "Region", "Name", "Amount"},
                             new String[]{"29.07.13", "West", "Joplin", "18707"},
                             new String[]{"30.07.13", "East", "Phyllis", "9939"}
                     );
+                } finally {
+                    is.close();
                 }
-            }
-            if (is != null) {
-                is.close();
-                is.close();
             }
         }
     }
-}
 
+    @Test
+    void zipXLSReaderTest() throws Exception {
+        InputStream is = classLoader.getResourceAsStream("Downloads.zip");
+        ZipInputStream zis = new ZipInputStream(is);
+        ZipFile zfile = new ZipFile(new File("src/test/resources/" + "Downloads.zip"));
+        ZipEntry entry;
+        while ((entry = zis.getNextEntry()) != null) {
+            if (entry.getName().contains("example.xlsx")) {
+                try (InputStream stream = zfile.getInputStream(entry)) {
+                    XLS xls = new XLS(stream);
+                    assertThat(
+                            xls.excel.getSheetAt(0)
+                                    .getRow(2)
+                                    .getCell(1)
+                                    .getStringCellValue()
+                    ).contains("East");
+                }
+            }
+        }
+
+    }
+
+
+    @Test
+    void zipPdfReader() throws Exception {
+        InputStream is = classLoader.getResourceAsStream("Downloads.zip");
+        ZipInputStream zip = new ZipInputStream(is);
+        ZipEntry entry;
+        ZipFile zfile = new ZipFile(new File("src/test/resources/" + "Downloads.zip"));
+        while ((entry = zip.getNextEntry()) != null) {
+            if (entry.getName().contains("new.pdf")) {
+                try (InputStream stream = zfile.getInputStream(entry)) {
+                    PDF pdf = new PDF(stream);
+                    assertThat(pdf.author).contains("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein");
+
+                }
+            }
+        }
+
+    }
+}
